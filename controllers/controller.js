31055b4727889bcffa2046ddff2338cadd804d40
hrahee200 <hrahee200@caledonian.ac.uk>
2021-04-14 15:45:29 +0100

    responsiveness and phase-1

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,39 +3,45 @@ var fs = require('fs')
 
 // Splash
 exports.renderPhase_1 = (req, res) => {
-  res.render('index')
+    res.render('index')
 }
 
 // Account
 exports.renderPhase_2 = (req, res) => {
-  res.render('index')
+    res.render('login')
+}
+exports.renderPhase_2_1 = (req, res) => {
+    res.render('signup')
+}
+exports.renderPhase_2_2 = (req, res) => {
+    res.render('editprof')
 }
 
 // Dashboard
-exports.renderPhase_3 = async function (req, res) {
-  await generate_game_data().then((data) => {
-    res.render('dashboard', {
-      demo: data,
+exports.renderPhase_3 = async function(req, res) {
+    await generate_game_data().then((data) => {
+        res.render('dashboard', {
+            demo: data,
+        })
     })
-  })
 }
 
 async function api_request(url) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      request(url, (err, response, body) => {
-        if (!err && response.statusCode < 400) {
-          resolve(JSON.parse(body))
-        }
-      })
-    }, 300)
-  })
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            request(url, (err, response, body) => {
+                if (!err && response.statusCode < 400) {
+                    resolve(JSON.parse(body))
+                }
+            })
+        }, 300)
+    })
 }
 
 const GameData = {
-  fps: {},
-  fantasy: {},
-  // horror: {},
+    fps: {},
+    fantasy: {},
+    // horror: {},
 }
 
 // const categories = {
@@ -46,148 +52,148 @@ const GameData = {
 
 // Debug version
 const categories = {
-  fps: ['10'],
-  fantasy: ['306130'],
+    fps: ['10'],
+    fantasy: ['306130'],
 }
 
 async function generate_game_data() {
-  const arr = Object.keys(categories)
+    const arr = Object.keys(categories)
 
-  console.log('\n--- Generating game data ---') //debug check max time to get response
-  console.log('MAX TIME: %i s', ((300 * (arr.length * categories[arr[0]].length)) / 1000) % 60)
+    console.log('\n--- Generating game data ---') //debug check max time to get response
+    console.log('MAX TIME: %i s', ((300 * (arr.length * categories[arr[0]].length)) / 1000) % 60)
 
-  for (let x = 0; x < arr.length; x++) {
-    //debug: Game Category being checked
-    console.log('\nCATEGORY:', arr[x])
+    for (let x = 0; x < arr.length; x++) {
+        //debug: Game Category being checked
+        console.log('\nCATEGORY:', arr[x])
 
-    for (let i = 0; i < categories[arr[x]].length; i++) {
-      var spyObj = await api_request('https://steamspy.com/api.php?request=appdetails&appid=' + categories[arr[x]][i])
-      var powObjInit = await api_request('https://store.steampowered.com/api/appdetails?appids=' + categories[arr[x]][i] + '&cc=gb&l=en&format=json')
-      var powObj = powObjInit[Object.keys(powObjInit)]['data']
+        for (let i = 0; i < categories[arr[x]].length; i++) {
+            var spyObj = await api_request('https://steamspy.com/api.php?request=appdetails&appid=' + categories[arr[x]][i])
+            var powObjInit = await api_request('https://store.steampowered.com/api/appdetails?appids=' + categories[arr[x]][i] + '&cc=gb&l=en&format=json')
+            var powObj = powObjInit[Object.keys(powObjInit)]['data']
 
-      //debug: Game ID that is called
-      console.log('I: %s/%i ID: %s', i < 10 ? '0' + i : i, categories[arr[x]].length - 1, categories[arr[x]][i])
+            //debug: Game ID that is called
+            console.log('I: %s/%i ID: %s', i < 10 ? '0' + i : i, categories[arr[x]].length - 1, categories[arr[x]][i])
 
-      const data = {
-        app_id: spyObj.appid,
-        name: spyObj.name,
-        developer: spyObj.developer,
-        publisher: spyObj.publisher,
-        languages: spyObj.languages,
-        website: powObj['website'],
-        background: powObj['background'],
-        header_image: powObj['header_image'],
-        release_date: powObj['release_date']['date'],
-        coming_soon: powObj['release_date']['coming_soon'],
-        price: check_if_free(powObj),
-        screenshots: get_images(powObj),
-        requirements: get_requirements(powObj),
-        support_info: get_support_info(powObj),
-        movies: get_movies(powObj),
-        description: get_description(powObj),
-        score: {
-          positive: spyObj.positive,
-          negative: spyObj.negative,
-        },
-      }
-      GameData[arr[x]][i] = data
+            const data = {
+                app_id: spyObj.appid,
+                name: spyObj.name,
+                developer: spyObj.developer,
+                publisher: spyObj.publisher,
+                languages: spyObj.languages,
+                website: powObj['website'],
+                background: powObj['background'],
+                header_image: powObj['header_image'],
+                release_date: powObj['release_date']['date'],
+                coming_soon: powObj['release_date']['coming_soon'],
+                price: check_if_free(powObj),
+                screenshots: get_images(powObj),
+                requirements: get_requirements(powObj),
+                support_info: get_support_info(powObj),
+                movies: get_movies(powObj),
+                description: get_description(powObj),
+                score: {
+                    positive: spyObj.positive,
+                    negative: spyObj.negative,
+                },
+            }
+            GameData[arr[x]][i] = data
+        }
     }
-  }
 
-  // let file = JSON.stringify(GameData, null, 2)
-  // fs.writeFileSync('data/game_data.json', file)
-  console.log(GameData)
-  return GameData
+    // let file = JSON.stringify(GameData, null, 2)
+    // fs.writeFileSync('data/game_data.json', file)
+    console.log(GameData)
+    return GameData
 }
 
 // Check if game is free
 function check_if_free(obj) {
-  const newObj = {
-    is_free: 'Paid',
-    currency: null,
-    initial: null,
-    final: null,
-    discount_percent: null,
-  }
+    const newObj = {
+        is_free: 'Paid',
+        currency: null,
+        initial: null,
+        final: null,
+        discount_percent: null,
+    }
 
-  if (obj['is_free'] == true) {
-    newObj.is_free = 'Free'
-  } else {
-    newObj.currency = obj['price_overview'].currency
-    newObj.initial = obj['price_overview'].initial
-    newObj.final = obj['price_overview'].final
-    newObj.discount_percent = obj['price_overview'].discount_percent
-  }
-  return newObj
+    if (obj['is_free'] == true) {
+        newObj.is_free = 'Free'
+    } else {
+        newObj.currency = obj['price_overview'].currency
+        newObj.initial = obj['price_overview'].initial
+        newObj.final = obj['price_overview'].final
+        newObj.discount_percent = obj['price_overview'].discount_percent
+    }
+    return newObj
 }
 
 // Get game images. Thumbnail & Full
 function get_images(obj) {
-  var arr = []
-  for (value in obj['screenshots']) {
-    var x = {
-      thumbnail: obj['screenshots'][value]['path_thumbnail'],
-      full: obj['screenshots'][value]['path_full'],
+    var arr = []
+    for (value in obj['screenshots']) {
+        var x = {
+            thumbnail: obj['screenshots'][value]['path_thumbnail'],
+            full: obj['screenshots'][value]['path_full'],
+        }
+        arr.push(x)
     }
-    arr.push(x)
-  }
-  return arr
+    return arr
 }
 
 // Get game support info. Email & Website
 function get_support_info(obj) {
-  const newObj = {
-    url: check_exists(obj['support_info'].url) && obj['support_info'].url > 0 ? obj['support_info'].url : null,
-    email: check_exists(obj['support_info'].email) && obj['support_info'].email > 0 ? obj['support_info'].email : null,
-  }
-  return newObj
+    const newObj = {
+        url: check_exists(obj['support_info'].url) && obj['support_info'].url > 0 ? obj['support_info'].url : null,
+        email: check_exists(obj['support_info'].email) && obj['support_info'].email > 0 ? obj['support_info'].email : null,
+    }
+    return newObj
 }
 
 // Get game trailer. Name & MP4
 function get_movies(obj) {
-  const newObj = {
-    name: check_exists(obj['movies']) ? obj['movies'][0]['name'] : null,
-    mp4: check_exists(obj['movies']) ? obj['movies'][0]['mp4'].max : null,
-  }
-  return newObj
+    const newObj = {
+        name: check_exists(obj['movies']) ? obj['movies'][0]['name'] : null,
+        mp4: check_exists(obj['movies']) ? obj['movies'][0]['mp4'].max : null,
+    }
+    return newObj
 }
 
 // Get game descriptions. Short, Long & About
 function get_description(obj) {
-  const newObj = {
-    short_desc: check_exists(obj['short_description']) ? strip_html(obj['short_description']) : null,
-    long_desc: check_exists(obj['detailed_description']) ? strip_html(obj['detailed_description']) : null,
-    about_desc: check_exists(obj['about_the_game']) ? strip_html(obj['about_the_game']) : null,
-  }
-  return newObj
+    const newObj = {
+        short_desc: check_exists(obj['short_description']) ? strip_html(obj['short_description']) : null,
+        long_desc: check_exists(obj['detailed_description']) ? strip_html(obj['detailed_description']) : null,
+        about_desc: check_exists(obj['about_the_game']) ? strip_html(obj['about_the_game']) : null,
+    }
+    return newObj
 }
 
 // Get game requirements. Minimum & Recommended
 function get_requirements(obj) {
-  const newObj = {
-    pc_req: {
-      minimum: check_exists(obj['pc_requirements'].minimum) ? strip_html(obj['pc_requirements'].minimum) : null,
-      recommended: check_exists(obj['pc_requirements'].recommended) ? strip_html(obj['pc_requirements'].recommended) : null,
-    },
-    mac_req: {
-      minimum: check_exists(obj['mac_requirements'].minimum) ? strip_html(obj['mac_requirements'].minimum) : null,
-      recommended: check_exists(obj['mac_requirements'].recommended) ? strip_html(obj['mac_requirements'].recommended) : null,
-    },
-  }
-  return newObj
+    const newObj = {
+        pc_req: {
+            minimum: check_exists(obj['pc_requirements'].minimum) ? strip_html(obj['pc_requirements'].minimum) : null,
+            recommended: check_exists(obj['pc_requirements'].recommended) ? strip_html(obj['pc_requirements'].recommended) : null,
+        },
+        mac_req: {
+            minimum: check_exists(obj['mac_requirements'].minimum) ? strip_html(obj['mac_requirements'].minimum) : null,
+            recommended: check_exists(obj['mac_requirements'].recommended) ? strip_html(obj['mac_requirements'].recommended) : null,
+        },
+    }
+    return newObj
 }
 
 // Check if input prop exists
 function check_exists(property) {
-  return typeof property == 'undefined' ? false : true
+    return typeof property == 'undefined' ? false : true
 }
 
 // Strip HTML tags from string
 function strip_html(obj) {
-  obj = obj.replace(/(<([^>]+)>)/gi, '')
-  obj = obj.replace(/\t/g, ' ')
-  obj = obj.replace(/\s{2,}/g, ' ').trim()
-  return obj // I dont understand regex
+    obj = obj.replace(/(<([^>]+)>)/gi, '')
+    obj = obj.replace(/\t/g, ' ')
+    obj = obj.replace(/\s{2,}/g, ' ').trim()
+    return obj // I dont understand regex
 }
 
 // try {
--- routes/route.js
+++ routes/route.js
@@ -6,19 +6,24 @@ const router = express.Router()
 // --- Routes --- //
 // Phase 1 - Index
 router.get('/', controller.renderPhase_1)
-// Phase 2 - Account
+    // Phase 2 - Account
 router.get('/login', controller.renderPhase_2)
+
+router.get('/signup', controller.renderPhase_2_1)
+
+router.get('/editprof', controller.renderPhase_2_2)
+
 // Phase 3 - Dashboard
 router.get('/dashboard', controller.renderPhase_3)
 
 // --- Errors --- //
 // 404 - Page not found
 router.use((req, res, next) => {
-  return res.status(404).send('Error 404 : Page Not Found')
-})
-// 500 - Any server error
+        return res.status(404).send('Error 404 : Page Not Found')
+    })
+    // 500 - Any server error
 router.use((req, res, next) => {
-  return res.status(500).send('Error 500 : Internal Server Error')
+    return res.status(500).send('Error 500 : Internal Server Error')
 })
 
 module.exports = router
